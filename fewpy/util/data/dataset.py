6f13bbd8c1ab7eeb06c8406472fffa75747ddc81
jzr-certi <jzr@certi.org.br>
2026-01-07 11:19:35 -0300

    dataset now treats annotations if needed

--- fewpy/util/data/dataset.py
+++ fewpy/util/data/dataset.py
@@ -8,10 +8,13 @@ from torchvision import transforms
 class FSLDataset(Dataset):
 
     def __init__(self,
-                 data: list[list[Image]],
+                 x: list[Image],
+                 s_x: list[Image]=None,
+                 s_y: list[dict]=None,
                  img_size: tuple=None,
                  max_size: int=None,
-                 pixel_norm: tuple=None) -> None:
+                 pixel_norm: tuple=None,
+                 norm_annot: bool=False) -> None:
         super().__init__()
 
         # set transform composition that turns pillow image objects into datapoints compatible with the library
@@ -28,13 +31,53 @@ class FSLDataset(Dataset):
             mean, std = pixel_norm
             transfs.append(transforms.Normalize(mean, std))
 
-        self.data = data
+        self.data = x
         self.transf = transforms.Compose(transfs)
+
+        self.support_set = (not s_x is None) and (not s_y is None)
+        self.s_x = s_x
+        self.s_y = s_y
+        self.support_set_preproc = False
+        self.norm_annot = norm_annot
 
     def __len__(self) -> int:
         return len(self.data)
     
-    def __getitem__(self, index: int) -> list[torch.Tensor]:
-        return torch.stack(list((self.transf(img) for img in self.data[index])))
- Meus itens
-Fewpy Models 
+    def __getitem__(self, index: int):
+
+        xi = self.transf(self.data[index])
+        if not self.support_set:
+            return xi
+        
+        if self.support_set_preproc:
+            return xi, self.s_x, self.s_y
+        
+        s_x = []
+        s_y = []
+        for img, annot in zip(self.s_x, self._s_y):
+            new_img = self.transf(img)
+
+            if self.norm_annot:
+                old_h, old_w = img.size
+
+                if "bboxes" not in annot.keys():
+                   raise KeyError("Bounding Box annotations should be named bboxes and be a list of bounding boxes [xmin, ymin, xmax, ymax]")
+
+                new_bboxes = []
+                for xmin, ymin, xmax, ymax in annot["bboxes"]:
+                    new_bboxes.append([
+                        xmin / old_w,
+                        ymin / old_h,
+                        xmax / old_w,
+                        ymax / old_h,
+                    ])
+                
+                annot["bboxes"] = new_bboxes
+            
+            s_y.append(annot)
+            s_x.append(new_img)
+
+        self.s_x = s_x
+        self.s_y = s_y
+
+        return xi, self.s_x, self.s_y
