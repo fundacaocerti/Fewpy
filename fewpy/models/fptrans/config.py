37203a5a8e01a965d482663ebfa3f8cb67f60d83
jzr-certi <jzr@certi.org.br>
2026-01-21 09:32:16 -0300

    defauçt values updated for pascal_VIT-B_fold9_1shot

--- fewpy/models/fptrans/config.py
+++ fewpy/models/fptrans/config.py
@@ -1,6 +1,28 @@
 from pydantic import BaseModel, Field
 from typing import Dict, Tuple
 
+args = {
+    "kshot": 1,
+    "dataset": "pascal",
+    "backbone": "VIT-B",
+    "split": 0,
+    "checkpoint": None,
+    "Probs_return": False,
+    "drop_dim": 1,
+    "drop_rate": 0.3,
+    "block_size": 16,
+    "height": 700,
+    "pretrained": "",
+    "SAHI": False,
+    "bg_num": 5,
+    "bsz": 32,
+    "img_size": 700,
+    "training": False,
+    "vit_depth": 10,
+    "vit_stride": 23,
+    "num_prompt": 12 * 6,
+}
+
 
 class FPTRANSConfig(BaseModel):
 
@@ -18,13 +40,13 @@ class FPTRANSConfig(BaseModel):
     pretrained: str = Field(default="")
     
     SAHI: bool = Field(default=False)
-    bg_num: int = Field(default=16)
+    bg_num: int = Field(default=5)
     bsz: int = Field(defaul=32)
     img_size: int = Field(default=700)
     training: bool = Field(default=False)
 
-    vit_depth: int | None = Field(default=12)
-    vit_stride: int = Field(default=1)
+    vit_depth: int | None = Field(default=10)
+    vit_stride: int = Field(default=23)
     coco2pascal: bool = Field(default=False)
-    num_prompt: int = Field(default=12)
+    num_prompt: int = Field(default=72)
     pt_std: float = Field(default=0.02)
