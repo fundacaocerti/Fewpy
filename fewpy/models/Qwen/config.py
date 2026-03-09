75c73684fd776d3ea24a801608c837f81aa1b596
jzr-certi <jzr@certi.org.br>
2026-03-09 10:00:56 -0300

    Revert "Documentation"

--- fewpy/models/Qwen/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import List
-
-
-class QwenConfig(BaseModel):
-
-    classnames: List[str] = Field(
-        description="Names of known and novel classes"
-    )
