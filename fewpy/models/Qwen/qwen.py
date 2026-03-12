39091e30d9937719cf764b06f7680a78bb7d4c9d
jzr-certi <jzr@certi.org.br>
2026-03-12 10:41:34 -0300

    ++first version of qwen fine tuning example

--- fewpy/models/Qwen/qwen.py
+++ fewpy/models/Qwen/qwen.py
@@ -7,8 +7,6 @@ from qwen_vl_utils import process_vision_info
 import PIL
 from typing import List, Dict
 
-from copy import deepcopy
-
 import supervision as sv
 import torch
 
@@ -32,7 +30,7 @@ class QwenWrapper:
             prompt = f"Outline the position of objects of class={single_cls}. Then output all the coordinates and classes of these objects in JSON format."
 
         messages = []
-        if (not s_x is None) and (s_y is None):
+        if (s_x is not None) and (s_y is not None):
             for example, annot in zip(s_x, s_y):
                 usr_msg = {
                     "role": "user",
@@ -50,11 +48,9 @@ class QwenWrapper:
                         }
                     ]
                 }
+                messages.extend([usr_msg, target_msg])
 
-                messages.append(usr_msg)
-                messages.append(target_msg)
-
-        results = []
+        batched_msgs = []
         for xi in x:
             task_msg = {
                 "role": "user",
@@ -63,34 +59,41 @@ class QwenWrapper:
                     {"type": "text", "text": prompt}
                 ],
             }
+            batched_msgs.append(messages + [task_msg])
 
-            msgs = deepcopy(messages)
-            msgs.append(task_msg)
+        image_inputs, video_inputs = process_vision_info(batched_msgs)
 
-            image_inputs, video_inputs = process_vision_info(msgs)
+        texts = [self.processor.apply_chat_template(
+            msgs, 
+            tokenize=False, 
+            add_generation_prompt=True
+        ) for msgs in batched_msgs]
 
-            text = self.processor.apply_chat_template(
-                msgs, 
-                tokenize=False, 
-                add_generation_prompt=True
-            )
+        inputs = self.processor(
+            text=texts,
+            images=image_inputs,
+            videos=video_inputs,
+            padding=True,
+            return_tensors="pt",
+        ).to("cuda")
 
-            inputs = self.processor(
-                text=[text],
-                images=image_inputs,
-                videos=video_inputs,
-                padding=True,
-                return_tensors="pt",
-            ).to("cuda")
+        if self.training:
 
-            with torch.inference_mode():
-                gen = self.model.generate(
-                    **inputs,
-                    max_new_tokens=256,
-            )
+            inputs["labels"] = inputs["input_ids"].clone()
+            outputs = self.model(**inputs)
 
-            trimmed = [g[len(i):] for i, g in zip(inputs.input_ids, gen)]
-            text = self.processor.batch_decode(trimmed, skip_special_tokens=True)[0]
+            return outputs.loss
+
+        with torch.inference_mode():
+            gen = self.model.generate(
+                **inputs,
+                max_new_tokens=256,
+        )
+
+        trimmed = [g[len(i):] for i, g in zip(inputs.input_ids, gen)]
+        decoded_texts = self.processor.batch_decode(trimmed, skip_special_tokens=True)[0]
+        results = []
+        for xi, text in zip(x, decoded_texts):
             image_results = []
             text = text.replace("```json", "").replace("```", "").strip()
             try:
@@ -108,9 +111,11 @@ class QwenWrapper:
                     if label is not None:
                         result["label"] = label
                     image_results.append(result)
-                results.append(image_results)
+                
             except Exception as e:
                 print("Output is not json compatible!", e)
+
+            results.append(image_results)
 
         return results
         
